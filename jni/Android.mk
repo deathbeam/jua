7081c8def028ccf3646854322c446563c3a264ec
Thomas Slusny <slusnucky@gmail.com>
2015-05-19 20:56:13 +0200

    Fixed lua_resume (changed in Lua 5.2)

--- jni/Android.mk
+++ jni/Android.mk
@@ -10,41 +10,41 @@ LOCAL_LDLIBS := -lm
 LOCAL_ARM_MODE  := arm
  
 LOCAL_SRC_FILES := luajava.c\
-	memcpy_wrap.c\
-	lua/lapi.c\
-	lua/lauxlib.c\
-	lua/lbaselib.c\
+  memcpy_wrap.c\
+  lua/lapi.c\
+  lua/lauxlib.c\
+  lua/lbaselib.c\
   lua/lbitlib.c\
-	lua/lcode.c\
+  lua/lcode.c\
   lua/lcorolib.c\
   lua/lctype.c\
-	lua/ldblib.c\
-	lua/ldebug.c\
-	lua/ldo.c\
-	lua/ldump.c\
-	lua/lfunc.c\
-	lua/lgc.c\
-	lua/linit.c\
-	lua/liolib.c\
-	lua/llex.c\
-	lua/lmathlib.c\
-	lua/lmem.c\
-	lua/loadlib.c\
-	lua/lobject.c\
-	lua/lopcodes.c\
-	lua/loslib.c\
-	lua/lparser.c\
-	lua/lstate.c\
-	lua/lstring.c\
-	lua/lstrlib.c\
-	lua/ltable.c\
-	lua/ltablib.c\
-	lua/ltm.c\
+  lua/ldblib.c\
+  lua/ldebug.c\
+  lua/ldo.c\
+  lua/ldump.c\
+  lua/lfunc.c\
+  lua/lgc.c\
+  lua/linit.c\
+  lua/liolib.c\
+  lua/llex.c\
+  lua/lmathlib.c\
+  lua/lmem.c\
+  lua/loadlib.c\
+  lua/lobject.c\
+  lua/lopcodes.c\
+  lua/loslib.c\
+  lua/lparser.c\
+  lua/lstate.c\
+  lua/lstring.c\
+  lua/lstrlib.c\
+  lua/ltable.c\
+  lua/ltablib.c\
+  lua/ltm.c\
   lua/lua.c\
   lua/luac.c\
-	lua/lundump.c\
+  lua/lundump.c\
   lua/lutf8lib.c\
-	lua/lvm.c\
-	lua/lzio.c
+  lua/lvm.c\
+  lua/lzio.c
  
 include $(BUILD_SHARED_LIBRARY)
