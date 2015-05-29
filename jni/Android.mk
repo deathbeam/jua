428e8ed77524b09e12c77c55fbbaaf94c46a9a15
Thomas Slusny <slusnucky@gmail.com>
2015-05-29 20:30:38 +0200

    Experimental support for LuaSocket

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,16 +1,17 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
  
-LOCAL_MODULE    := luajava
-LOCAL_C_INCLUDES := lua
+LOCAL_MODULE     := nonlua
+LOCAL_C_INCLUDES := lua\
+  luasocket
  
 LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__ -std=c99 -DFT2_BUILD_LIBRARY
 LOCAL_CPPFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__ -std=c99 -DFT2_BUILD_LIBRARY
 LOCAL_LDLIBS := -lm
 LOCAL_ARM_MODE  := arm
  
-LOCAL_SRC_FILES := luajava.c\
-  memcpy_wrap.c\
+LOCAL_SRC_FILES := memcpy_wrap.c\
+  luajava.c\
   lua/lapi.c\
   lua/lauxlib.c\
   lua/lbaselib.c\
@@ -44,6 +45,22 @@ LOCAL_SRC_FILES := luajava.c\
   lua/lundump.c\
   lua/lutf8lib.c\
   lua/lvm.c\
-  lua/lzio.c
- 
+  lua/lzio.c\
+  luasocket/auxiliar.c\
+  luasocket/buffer.c\
+  luasocket/except.c\
+  luasocket/inet.c\
+  luasocket/io.c\
+  luasocket/luasocket.c\
+  luasocket/mime.c\
+  luasocket/options.c\
+  luasocket/select.c\
+  luasocket/serial.c\
+  luasocket/tcp.c\
+  luasocket/timeout.c\
+  luasocket/udp.c\
+  luasocket/unix.c\
+  luasocket/usocket.c\
+  luasocket/wsocket.c
+
 include $(BUILD_SHARED_LIBRARY)
