d7c34653936ce771e42e6ff9cf1b828be4f71fcb
Thomas Slusny <slusnucky@gmail.com>
2015-05-29 22:05:28 +0200

    Replaced luasocket with ssocket because of cross-platform stability

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
  
 LOCAL_MODULE     := nonlua
 LOCAL_C_INCLUDES := lua\
-  luasocket
+  socket
  
 LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__ -std=c99 -DFT2_BUILD_LIBRARY
 LOCAL_CPPFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__ -std=c99 -DFT2_BUILD_LIBRARY
@@ -46,21 +46,8 @@ LOCAL_SRC_FILES := memcpy_wrap.c\
   lua/lutf8lib.c\
   lua/lvm.c\
   lua/lzio.c\
-  luasocket/auxiliar.c\
-  luasocket/buffer.c\
-  luasocket/except.c\
-  luasocket/inet.c\
-  luasocket/io.c\
-  luasocket/luasocket.c\
-  luasocket/mime.c\
-  luasocket/options.c\
-  luasocket/select.c\
-  luasocket/serial.c\
-  luasocket/tcp.c\
-  luasocket/timeout.c\
-  luasocket/udp.c\
-  luasocket/unix.c\
-  luasocket/usocket.c\
-  luasocket/wsocket.c
+  socket/buffer.c\
+  socket/socket.c\
+  socket/timeout.c
 
 include $(BUILD_SHARED_LIBRARY)
