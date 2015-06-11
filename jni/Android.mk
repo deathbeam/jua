df224b77439bdc9020c3738ba2f2bb53c9a8202b
Thomas Slusny <slusnucky@gmail.com>
2015-06-11 18:08:24 +0200

    Refactor and rewrite of native backend

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,65 +2,66 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
  
 LOCAL_MODULE    := nonlua
-LOCAL_C_INCLUDES := lua luasocket 
+LOCAL_C_INCLUDES := nonlua lua luasocket 
  
 LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
 LOCAL_CPPFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
 LOCAL_LDLIBS := -lm
 LOCAL_ARM_MODE  := arm
  
-LOCAL_SRC_FILES := io.nondev.nonlua.Lua.cpp\
-	lua/lapi.c\
-	lua/lauxlib.c\
-	lua/lbaselib.c\
-	lua/lbitlib.c\
-	lua/lcode.c\
-	lua/lcorolib.c\
-	lua/lctype.c\
-	lua/ldblib.c\
-	lua/ldebug.c\
-	lua/ldo.c\
-	lua/ldump.c\
-	lua/lfunc.c\
-	lua/lgc.c\
-	lua/linit.c\
-	lua/liolib.c\
-	lua/llex.c\
-	lua/lmathlib.c\
-	lua/lmem.c\
-	lua/loadlib.c\
-	lua/lobject.c\
-	lua/lopcodes.c\
-	lua/loslib.c\
-	lua/lparser.c\
-	lua/lstate.c\
-	lua/lstring.c\
-	lua/lstrlib.c\
-	lua/ltable.c\
-	lua/ltablib.c\
-	lua/ltm.c\
-	lua/lua.c\
-	lua/lundump.c\
-	lua/lutf8lib.c\
-	lua/lvm.c\
-	lua/lzio.c\
-	luasocket/auxiliar.c\
-	luasocket/buffer.c\
-	luasocket/except.c\
+LOCAL_SRC_FILES := luasocket/udp.c\
+	luasocket/timeout.c\
 	luasocket/inet.c\
+	luasocket/tcp.c\
 	luasocket/io.c\
+	luasocket/options.c\
 	luasocket/luasocket.c\
 	luasocket/mime.c\
-	luasocket/options.c\
 	luasocket/select.c\
-	luasocket/tcp.c\
-	luasocket/timeout.c\
-	luasocket/udp.c\
-	luasocket/unix.c\
-	luasocket/usocket.c\
+	luasocket/except.c\
 	luasocket/wsocket.c\
+	luasocket/unix.c\
+	luasocket/auxiliar.c\
+	luasocket/buffer.c\
+	luasocket/usocket.c\
+	lua/loadlib.c\
+	lua/ltm.c\
+	lua/lbitlib.c\
+	lua/lzio.c\
+	lua/lauxlib.c\
+	lua/linit.c\
+	lua/lbaselib.c\
+	lua/lfunc.c\
+	lua/ltablib.c\
+	lua/lmem.c\
+	lua/lstring.c\
+	lua/lopcodes.c\
+	lua/lutf8lib.c\
+	lua/lctype.c\
+	lua/ldo.c\
+	lua/llex.c\
+	lua/ldblib.c\
+	lua/lvm.c\
+	lua/lobject.c\
+	lua/lmathlib.c\
+	lua/ldebug.c\
+	lua/liolib.c\
+	lua/ldump.c\
+	lua/loslib.c\
+	lua/lapi.c\
+	lua/lcode.c\
+	lua/lstate.c\
+	lua/lgc.c\
+	lua/lua.c\
+	lua/lundump.c\
+	lua/lparser.c\
+	lua/lstrlib.c\
+	lua/ltable.c\
+	lua/lcorolib.c\
 	memcpy_wrap.c\
-	nonlua.cpp\
-	nonsocket.cpp
+	nonlua/nljavalib.cpp\
+	nonlua/nlsocklib.cpp\
+	nonlua/nonlua.cpp\
+	io.nondev.nonlua.Lua.cpp
  
 include $(BUILD_SHARED_LIBRARY)
