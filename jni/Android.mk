03ba144c51a5581b3b4080ec4090fe528bd52876
Thomas Slusny <slusnucky@gmail.com>
2015-06-14 14:26:26 +0200

    Replaced Lua 5.3 with LuaJIT 2.0.3

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,4 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := luajit
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../ext/$(TARGET_ARCH_ABI)/libluajit.a
+include $(PREBUILT_STATIC_LIBRARY)
+
 include $(CLEAR_VARS)
  
 LOCAL_MODULE    := nonlua
@@ -8,6 +15,7 @@ LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
 LOCAL_CPPFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
 LOCAL_LDLIBS := -lm
 LOCAL_ARM_MODE  := arm
+LOCAL_STATIC_LIBRARIES := luajit
  
 LOCAL_SRC_FILES := luasocket/udp.c\
 	luasocket/timeout.c\
@@ -24,40 +32,6 @@ LOCAL_SRC_FILES := luasocket/udp.c\
 	luasocket/auxiliar.c\
 	luasocket/buffer.c\
 	luasocket/usocket.c\
-	lua/loadlib.c\
-	lua/ltm.c\
-	lua/lbitlib.c\
-	lua/lzio.c\
-	lua/lauxlib.c\
-	lua/linit.c\
-	lua/lbaselib.c\
-	lua/lfunc.c\
-	lua/ltablib.c\
-	lua/lmem.c\
-	lua/lstring.c\
-	lua/lopcodes.c\
-	lua/lutf8lib.c\
-	lua/lctype.c\
-	lua/ldo.c\
-	lua/llex.c\
-	lua/ldblib.c\
-	lua/lvm.c\
-	lua/lobject.c\
-	lua/lmathlib.c\
-	lua/ldebug.c\
-	lua/liolib.c\
-	lua/ldump.c\
-	lua/loslib.c\
-	lua/lapi.c\
-	lua/lcode.c\
-	lua/lstate.c\
-	lua/lgc.c\
-	lua/lua.c\
-	lua/lundump.c\
-	lua/lparser.c\
-	lua/lstrlib.c\
-	lua/ltable.c\
-	lua/lcorolib.c\
 	memcpy_wrap.c\
 	nonlua/nljavalib.cpp\
 	nonlua/nlsocklib.cpp\
