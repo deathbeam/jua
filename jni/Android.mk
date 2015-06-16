df2772146ac475048266c828f9535d7d56cfd7b9
Thomas Slusny <slusnucky@gmail.com>
2015-06-16 03:19:36 +0200

    Correct builds finally

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,19 +3,19 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := luajit
-LOCAL_SRC_FILES := $(LOCAL_PATH)/../ext/$(TARGET_ARCH_ABI)/libluajit.a
-include $(PREBUILT_STATIC_LIBRARY)
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../libs/$(TARGET_ARCH_ABI)/libluajit.so
+include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
  
 LOCAL_MODULE    := nonlua
-LOCAL_C_INCLUDES := nonlua lua luasocket 
+LOCAL_C_INCLUDES := nonlua luajit sdl luasocket 
  
 LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
 LOCAL_CPPFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
 LOCAL_LDLIBS := -lm
 LOCAL_ARM_MODE  := arm
-LOCAL_STATIC_LIBRARIES := luajit
+LOCAL_SHARED_LIBRARIES := luajit
  
 LOCAL_SRC_FILES := luasocket/udp.c\
 	luasocket/timeout.c\
@@ -36,6 +36,13 @@ LOCAL_SRC_FILES := luasocket/udp.c\
 	nonlua/nljavalib.cpp\
 	nonlua/nlsocklib.cpp\
 	nonlua/nonlua.cpp\
-	io.nondev.nonlua.Lua.cpp
+	io.nondev.nonlua.Lua.cpp\
+	common/video.c\
+	common/surface.c\
+	common/variant.c\
+	common/common.c\
+	common/table.c\
+	common/rwops.c\
+	common/array.c
  
 include $(BUILD_SHARED_LIBRARY)
