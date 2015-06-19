464fac0ca0b6ad0e35a6d06ec64d70a2e86fbbb4
Thomas Slusny <slusnucky@gmail.com>
2015-06-19 11:39:40 +0200

    Removed LuaSocket and SDL, replaced LuaJIT unstable with stable

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,13 +3,13 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := luajit
-LOCAL_SRC_FILES := $(LOCAL_PATH)/../libs/$(TARGET_ARCH_ABI)/libluajit.so
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../libs/android32/$(TARGET_ARCH_ABI)/libluajit.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
  
 LOCAL_MODULE    := nonlua
-LOCAL_C_INCLUDES := nonlua luajit luasocket 
+LOCAL_C_INCLUDES := nonlua luajit 
  
 LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
 LOCAL_CPPFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
@@ -17,24 +17,8 @@ LOCAL_LDLIBS := -lm
 LOCAL_ARM_MODE  := arm
 LOCAL_SHARED_LIBRARIES := luajit
  
-LOCAL_SRC_FILES := luasocket/udp.c\
-	luasocket/timeout.c\
-	luasocket/inet.c\
-	luasocket/tcp.c\
-	luasocket/io.c\
-	luasocket/options.c\
-	luasocket/luasocket.c\
-	luasocket/mime.c\
-	luasocket/select.c\
-	luasocket/except.c\
-	luasocket/wsocket.c\
-	luasocket/unix.c\
-	luasocket/auxiliar.c\
-	luasocket/buffer.c\
-	luasocket/usocket.c\
-	memcpy_wrap.c\
+LOCAL_SRC_FILES := memcpy_wrap.c\
 	nonlua/nljavalib.cpp\
-	nonlua/nlsocklib.cpp\
 	nonlua/nonlua.cpp\
 	io.nondev.nonlua.Lua.cpp
  
